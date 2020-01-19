555eab528e1aa41b789fb794355dec01889931fa
Siarhei <swarog2002@mail.ru>
2020-01-19 13:39:27 +0300

    Create AddRemoveContentItem component

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Prepare simple speech cards
+
+###TODO:
+
+- [ ] Move margin-bottom from a form field to a form-group wrapper
+- [ ] Add possibility to create/remove new input for a step content instead of textarea 
+- [ ] Add tests
--- src/components/NewSpeech/MainStep.tsx
+++ src/components/NewSpeech/MainStep.tsx
@@ -17,46 +17,50 @@ export const MainStep: FunctionComponent<MainStepProps> = ({
   title,
 }) => {
   return (
-    <div className="form-group">
-      <label htmlFor="step-title">Title:</label>
-      <input
-        id="step-title"
-        name="title"
-        type="text"
-        autoCapitalize="none"
-        autoCorrect="off"
-        placeholder={`Enter step ${step} title`}
-        onChange={handleChange}
-        spellCheck={false}
-        value={title}
-      />
-      <label htmlFor="step-content">Content:</label>
-      <span id="step-content-hint" className="step-content-hint">
-        Markdown&nbsp;
-        <a
-          href="https://www.markdownguide.org/basic-syntax"
-          target="_blank"
-          tabIndex={-1}
-        >
-          syntax
-        </a>
-        &nbsp;is available.
-      </span>
-      <textarea
-        id="step-content"
-        name="content"
-        rows={5}
-        aria-describedby="step-content-hint"
-        autoCapitalize="none"
-        autoCorrect="off"
-        placeholder={`Use an asterisk before each item.
+    <>
+      <div className="form-group">
+        <label htmlFor="step-title">Title:</label>
+        <input
+          id="step-title"
+          name="title"
+          type="text"
+          autoCapitalize="none"
+          autoCorrect="off"
+          placeholder={`Enter step ${step} title`}
+          onChange={handleChange}
+          spellCheck={false}
+          value={title}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="step-content">Content:</label>
+        <span id="step-content-hint" className="step-content-hint">
+          Markdown&nbsp;
+          <a
+            href="https://www.markdownguide.org/basic-syntax"
+            target="_blank"
+            tabIndex={-1}
+          >
+            syntax
+          </a>
+          &nbsp;is available.
+        </span>
+        <textarea
+          id="step-content"
+          name="content"
+          rows={5}
+          aria-describedby="step-content-hint"
+          autoCapitalize="none"
+          autoCorrect="off"
+          placeholder={`Use an asterisk before each item.
 New item should start from a new line:
 * Some idea
 * Next idea`}
-        onChange={handleChange}
-        spellCheck={false}
-        value={content}
-      />
-    </div>
+          onChange={handleChange}
+          spellCheck={false}
+          value={content}
+        />
+      </div>
+    </>
   );
 };
