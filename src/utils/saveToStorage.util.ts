cc0e2a8f3b3a8ceb79857e60d1527a57360dcb0b
Siarhei_Vaitehovich <siarhei_vaitehovich@epam.com>
2020-06-05 18:03:38 +0300

    Update a new speech name in case it's already exists

--- src/utils/saveToStorage.util.ts
+++ src/utils/saveToStorage.util.ts
@@ -1,8 +1,17 @@
 import { IState } from 'types';
 import { prepareSpeechName } from './prepareSpeechName.util';
+import { getSpeechNamesFromStorage } from './getSpeechNamesFromStorage.util';
 
-export const saveToStorage = (data: IState): void =>
+export const saveToStorage = (data: IState): void => {
+  const speechesList = getSpeechNamesFromStorage();
+  let name = data.name;
+
+  if (speechesList.includes(`speech_${name}`)) {
+    name = `new_${name}`;
+  }
+
   localStorage.setItem(
-    `speech_${prepareSpeechName(data.name)}`,
+    `speech_${prepareSpeechName(name)}`,
     JSON.stringify(data)
   );
+};
