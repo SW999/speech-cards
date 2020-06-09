3864f7dd1980a79ae213ba1a1bd9485497afa56f
Siarhei_Vaitehovich <siarhei_vaitehovich@epam.com>
2020-06-09 17:47:06 +0300

    Get rid of BrowserRouter, cause I'm fucked with it

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
 // <reference path='../custom.d.ts'/>
 import * as React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
+import { HashRouter } from 'react-router-dom';
 import './scss/style.scss';
 import { App } from './App';
-require('dotenv').config();
-
-const history = createHistory({
-  basename: process.env.PUBLIC_URL,
-});
 
 render(
-  <BrowserRouter history={history}>
+  <HashRouter basename="/">
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('app')
 );
