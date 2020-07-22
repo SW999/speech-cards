990150073556a4a20033c937134cd9a4270047c8
Siarhei_Vaitehovich <siarhei_vaitehovich@epam.com>
2020-07-22 23:45:14 +0300

    An Experiment

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 // <reference path='../custom.d.ts'/>
 import * as React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './scss/style.scss';
 import { App } from './App';
 
 render(
-  <Router basename="/speech-cards">
+  <BrowserRouter>
     <App />
-  </Router>,
+  </BrowserRouter>,
   document.getElementById('app')
 );
