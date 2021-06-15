233b05c8f2af822d79e874816f37e224b99c5f31
Siarhei_Vaitehovich <siarhei_vaitehovich@epam.com>
2021-06-15 13:05:22 +0300

    Update dependencies

--- src/components/with-loader/WithLoader.tsx
+++ src/components/with-loader/WithLoader.tsx
@@ -1,16 +1,18 @@
 import React, { Suspense } from 'react';
 import Loader from '~components/loader/Loader';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const WithLoader = ({ component: Component, ...rest }) => (props?: any) => {
-  const { onlyText, placeholder, ..._rest } = rest;
-  return (
-    <Suspense
-      fallback={<Loader onlyText={onlyText} placeholder={placeholder} />}
-    >
-      <Component {..._rest} {...props} />
-    </Suspense>
-  );
-};
+const WithLoader =
+  ({ component: Component, ...rest }) =>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (props?: any) => {
+    const { onlyText, placeholder, ..._rest } = rest;
+    return (
+      <Suspense
+        fallback={<Loader onlyText={onlyText} placeholder={placeholder} />}
+      >
+        <Component {..._rest} {...props} />
+      </Suspense>
+    );
+  };
 
 export default WithLoader;
